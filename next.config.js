20fb110e55a9622c03f1c44d20f6a92938ff1f8f
Patryk Jaseniuk <patryk.jaseniuk@gmail.com>
2022-11-23 12:27:36 +0100

    output w docs

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  basePath: '/StatkiOut',
 }
 
 module.exports = nextConfig
